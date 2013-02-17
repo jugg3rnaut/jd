5719dda82518fc462c815f80028cdb48f1dcd75e
Arit Mondal <aritmondal@gmail.com>
2013-02-18 02:07:55 +0530

    Generated JD Rails 3 App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jd::Application.config.secret_token = '11e2facedf6adff99b706e2866fa93cc1effe82b1f2818e961749b079fd8f057adeead31f2f5805e82f13cc307d3f512d019218bf8977ef7569027fb0502889e'
